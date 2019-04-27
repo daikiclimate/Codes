d066b40be0902b338ab9f1c8821ceb4714093c3e
daikiclimate <daikiclimate041@gmail.com>
2019-04-27 23:33:41 +0900

    done

--- Mnist_conv.py
+++ Mnist_conv.py
@@ -4,6 +4,12 @@ import torchvision.transforms as transforms
 import numpy as np
 import torch.optim as optim
 import torch.nn as nn
+import numpy as np
+batch_size = 128
+
+class Flatten(nn.Module):
+    def forward(self, input):
+        return input.view(input.size(0), -1)
 
 #トレインデータ、テストデータのロード
 transform = transforms.Compose(
@@ -14,7 +20,7 @@ trainset = torchvision.datasets.MNIST(root='./data',
                                         download=True,
                                         transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset,
-                                            batch_size=64,
+                                            batch_size=batch_size,
                                             shuffle=True)
 
 testset = torchvision.datasets.MNIST(root='./data', 
@@ -22,16 +28,20 @@ testset = torchvision.datasets.MNIST(root='./data',
                                         download=True, 
                                         transform=transform)
 testloader = torch.utils.data.DataLoader(testset, 
-                                            batch_size=64,
+                                            batch_size=batch_size,
                                             shuffle=False)
-#ハイパーパラメータ
-D_in, H, D_out  = 28*28, 50, 10
 
 #モデルの定義
 model = torch.nn.Sequential(
-    torch.nn.Linear(D_in, H),
-    torch.nn.ReLU(),
-    torch.nn.Linear(H, D_out),
+    nn.Conv2d(1, 8, 5),  # 28 * 28 * 16-> 24 * 24 * 16
+    nn.ReLU(),
+    nn.MaxPool2d(2), #24 * 24 *16 -> 12 * 12 * 16    
+    nn.Conv2d(8, 16,  5), # 12* 12 * 16 -> 8* 8 * 32
+    nn.ReLU(),
+    nn.Dropout2d(),
+    Flatten(),
+    nn.Linear(8 * 8 * 16, 128),
+    nn.Linear(128, 10)
 )
 
 #勾配法
@@ -39,13 +49,14 @@ optimizer = optim.SGD(model.parameters(), lr=0.01)
 #誤差関数
 criterion = nn.CrossEntropyLoss()
 
+training_loss = []
+
 #モデルの学習
 model.train()
 for i in range(10):
     runnning_loss = 0.0
     for j, data in enumerate(trainloader):
-        train_data, teacher_labels = data
-        inputs = train_data.reshape(-1, 28*28)
+        inputs, teacher_labels = data
         model.zero_grad()
         outputs = model(inputs)    
         
@@ -58,10 +69,11 @@ for i in range(10):
         
         #途中結果の表示
         #バッチサイズに合わせて変更する必要あり
-        if j % 500 == 499:
-            print(i, j+1, runnning_loss/2000)
+        if j % 100 == 99:
+            print("[{:d}, {:d} loss : {:.3f}]".format(i, j+1, runnning_loss/2000))
             runnning_loss = 0.0
-           
+    training_loss.append(loss)
+      
 count_when_correct = 0
 total = 0
 
@@ -70,7 +82,6 @@ for data in testloader:
   test_data, test_labels = data
   
   #テストデータの推論
-  test_data = test_data.reshape(-1, 28*28)
   outputs = model(test_data)
   _, predicted = torch.max(outputs.data, 1)
   #正答率の算出
@@ -78,4 +89,3 @@ for data in testloader:
   count_when_correct += (predicted == test_labels).sum()
     
 print('正解率：%d / %d => %.1f'% (count_when_correct, total, int(count_when_correct)/int(total)*100 ),"%")
-
